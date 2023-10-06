b32ede75e5d79da19b8bce9fbff2729faa5a1904
xu8866 <116983556+xu8866@users.noreply.github.com>
2023-10-06 10:13:02 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>阿俊个人网页</title>
+    <link rel="stylesheet" href="css/index.css">
+</head>
+
+<body>
+    <div class="content">
+        <img src="img/ch.jpg" alt="">
+        <div class="content_r clearfix">
+            <div class="content_l clearfix">
+                <h2>Hi~ 我是嘎比， Welcome here ！</h2>
+                <p class="cc">欢迎来到我的主页，一位热爱编程的小渣渣~</p>
+                <p class="cc">爱好计算机，会去自学一些感兴趣的东西 !</p>
+                <br>
+                <p class="color_1">I am an ambitious salted fish!</p>
+                <p class="color_1">我是一位有野心的咸鱼!</p>
+                <br>
+                <p class="deeppink">只要路是对的，就不怕路远。</p>
+                <p class="deeppink">As long as the road is right, you are not afraid of the distance.</p>
+                <div class="link">
+                    <a href="https://wep.vipyshy.com/" class="dodgerblue" target="_blank">官网</a>
+                    <a href="/" class="deeppink_1" target="_blank">QQ群</a>
+                    <a href="https://wep.vipyshy.com/" class="magenta" target="_blank">五行资源博客</a>
+                    <a href="https://music.163.com/" class="orange" target="_blank">网易云音乐</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
